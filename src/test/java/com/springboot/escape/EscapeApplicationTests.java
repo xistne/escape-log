d9a39ee250d1963133e9add3f6f080296d012e7a
xistne <du20063@gmail.com>
2025-03-11 16:34:42 +0900

    Feat : 프로젝트 생성

---
+++ src/test/java/com/springboot/escape/EscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
