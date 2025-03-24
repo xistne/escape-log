2497d8cd934dee5a0c26dfcd4c00346b9883b67e
xistne <du20063@gmail.com>
2025-03-24 18:56:58 +0900

    Feat : Global Exception Handler

---
+++ src/main/java/com/springboot/escape/controller/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.springboot.escape.controller;
+
+import com.springboot.escape.data.dto.ApiResponse;
+import com.springboot.escape.exception.AuthException;
+import com.springboot.escape.exception.UserException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ApiResponse<Void>> handleAuthException(AuthException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().defaultHttpStatus())
+                .body(ApiResponse.failure(e));
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ApiResponse<Void>> handleUserException(UserException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().defaultHttpStatus())
+                .body(ApiResponse.failure(e));
+    }
+}
