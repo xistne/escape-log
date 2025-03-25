e64a85d8c20804b3bd63510e20ce1484dc2eb9ba
xistne <du20063@gmail.com>
2025-03-25 17:27:53 +0900

    Fix : swagger 오류 수정

--- src/main/java/com/springboot/escape/controller/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/escape/controller/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.springboot.escape.controller;
 import com.springboot.escape.data.dto.ApiResponse;
 import com.springboot.escape.exception.AuthException;
 import com.springboot.escape.exception.UserException;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<ApiResponse<Void>> handleAuthException(AuthException e) {
