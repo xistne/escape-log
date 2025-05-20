e8fcac7f59ecd7fff9535542da773df19df43407
xistne <du20063@gmail.com>
2025-05-20 17:33:51 +0900

    Fix : @Configuration 누락 수정

--- backend/src/main/java/com/springboot/escape/configuration/CorsConfig.java
+++ backend/src/main/java/com/springboot/escape/configuration/CorsConfig.java
@@ -1,12 +1,14 @@
 package com.springboot.escape.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+@Configuration
 public class CorsConfig {
 
     @Bean
